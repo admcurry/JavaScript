066cf9a80c80ea9f58e955859f5f06dd0923ea8a
admcurry <adambcurry@outlook.com>
2023-04-08 19:24:44 +0100

    Years converter calc

---
+++ StarterProjects/02_years_calc/main.js
@@ -0,0 +1,10 @@
+let myAge = 20
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = (myAge - 2) * 4
+
+myAgeInDogYears = earlyYears + laterYears
+
+let myName = 'Adam'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
