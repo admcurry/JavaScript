a9b653374f3fb40f6689ae3616f721e9822e1cef
admcurry <adambcurry@outlook.com>
2023-04-08 15:59:51 +0100

    Temperature converter calc

---
+++ GuidedProjects/01_temperature_calc/app.js
@@ -0,0 +1,7 @@
+let kelvin = 293 //todays temperature
+
+let celsius = kelvin - 273  //convert to celsius
+
+let fahrenheit = Math.floor(celsius * (9/5) + 32) //convert to fahrenheit
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
