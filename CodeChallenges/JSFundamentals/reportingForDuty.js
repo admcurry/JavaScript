b6832687106a947d483fe0666076956656d15ff8
admcurry <adambcurry@outlook.com>
2023-04-10 16:30:05 +0100

    Code Challenges: JavaScript Fundamentals

---
+++ CodeChallenges/JSFundamentals/reportingForDuty.js
@@ -0,0 +1,8 @@
+/*
+Write a function, reportingForDuty(), 
+that has two string parameters, rank and lastName, 
+and returns a string in the following format:
+‘rank lastName reporting for duty!’
+*/
+
+function reportingForDuty(rank, lastName) {return `${rank} ${lastName} reporting for duty!`} 
