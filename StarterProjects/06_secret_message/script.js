eb46ef2f8b9881519b2eb0d5047d050a1f8c5c69
admcurry <adambcurry@outlook.com>
2023-04-10 15:55:26 +0100

    Arrays work

---
+++ StarterProjects/06_secret_message/script.js
@@ -0,0 +1,1 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
