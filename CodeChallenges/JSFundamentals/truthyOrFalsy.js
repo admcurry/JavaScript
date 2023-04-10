a53316da3496e345bd7df8bb333d573400f4cb5f
admcurry <adambcurry@outlook.com>
2023-04-10 16:30:13 +0100

    Code Challenges: JavaScript Fundamentals

---
+++ CodeChallenges/JSFundamentals/truthyOrFalsy.js
@@ -0,0 +1,10 @@
+/*
+It can be hard to keep track of what’s truthy or falsy in JavaScript.
+Write a function, truthyOrFalsy(), that takes in any value and 
+returns true if that value is truthy and false if that value is falsy.
+*/
+
+function truthyOrFalsy(input) {
+  if (input) {return true}
+  else {return false}
+}
