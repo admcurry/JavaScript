b326276ab220d521b33687991ef197a0348ec734
admcurry <adambcurry@outlook.com>
2023-04-10 16:29:47 +0100

    Code Challenges: JavaScript Fundamentals

---
+++ CodeChallenges/JSFundamentals/agreeOrDisagree.js
@@ -0,0 +1,10 @@
+/*
+Write a function, agreeOrDisagree(), that takes in two strings, 
+and returns 'You agree!' if the two strings are the same and 'You disagree!'
+if the two strings are different.
+ */
+
+function agreeOrDisagree(s1, s2) {
+  if (s1 === s2) {return 'You agree!'}
+  else {return 'You disagree!'}
+}
