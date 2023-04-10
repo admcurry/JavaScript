5d989a47d6c1f8b369cfaae5e05bf9b2f741f66d
admcurry <adambcurry@outlook.com>
2023-04-10 16:04:54 +0100

    Arrays

--- StarterProjects/06_secret_message/script.js
+++ StarterProjects/06_secret_message/script.js
@@ -1,1 +1,11 @@
-let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript']
+
+secretMessage.pop()
+secretMessage.push('to', 'Program')
+secretMessage[7] = 'right'
+secretMessage.shift()
+secretMessage.unshift('Programming')
+secretMessage.splice(6, 5)
+secretMessage.splice(6, 0, 'know')
+
+console.log(secretMessage.join().replace(/,/g, ' '))
